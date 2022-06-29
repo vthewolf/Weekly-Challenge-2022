583344696fce6afc2454b294c7d918a4e6269fbf
wolf <vsantiagoferrera@gmail.com>
2022-06-29 13:39:21 +0200

    Nuevos retos
    Añadido reto #1 y reto #2 pero sin hacer

--- Retos/src/FizzBuzz.java
+++ Retos/src/FizzBuzz.java
@@ -1,3 +1,12 @@
+/*Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Dificultad: Facil
+ * Enunciado: Escribe un programa que muestre por consola los números del 1 al 100.
+ * - Múltiplos de 3: Se sustituyen por la palabra "Fizz"
+ * - Múltiplos de 5: Se sustituyen por la palabra "Buzz"
+ * - Múltiplos de 3 y 5: Se sustituyen por la palabra "FizzBuzz"
+ */
+
 public class FizzBuzz {
     public static void main(String[] args) throws Exception {
         int i =1;
