77f3e01c3b93a778d02dae9add639dcb2ee0f70c
wolf <vsantiagoferrera@gmail.com>
2022-06-29 14:55:29 +0200

    Fibonacci completado

---
+++ Retos/src/Anagrama.java
@@ -0,0 +1,16 @@
+/* RETO #1 ¿ES UN ANAGRAMA?
+ * DIFICULTAD: MEDIA
+ * Enunciado: Escribe una función que reciba dos palabras y retorne verdadero o falso.
+ * Un anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales NO son anagrama
+ */
+
+
+
+public class Anagrama {
+      
+    
+    
+
+}
---
+++ Retos/src/Fibonacci.java
@@ -0,0 +1,24 @@
+/*Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * DIFICULTAD: DIFICIL
+ * ENUNCIADO: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando por el 0.
+ * La serid de Fibonacci se compone de una sucesión de números en la que el siguiente siempre es la suma del anterior.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+ */
+
+ public class Fibonacci {
+    
+   public static void main(String[] args) throws Exception { 
+      long n1=0;
+      long n2=1;
+      int i=0;
+
+      for (i=0; i<=50; i++){
+         System.out.println(n1);
+         long fib = n1+n2;
+         n1=n2;
+         n2=fib;
+         
+      }
+   }
+}
